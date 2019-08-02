6d9511ed5930f7b2cbaf4cf86714c2b133f2e00f
Gilson <gilsonjuniorpro@gmail.com>
2019-08-02 00:59:18 -0400

    .

--- app/src/androidTest/java/com/wattpad/ca/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/wattpad/ca/ExampleInstrumentedTest.kt
@@ -1,12 +1,20 @@
 package com.wattpad.ca
 
 import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
-
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.wattpad.ca.controller.StoryController
+import com.wattpad.ca.dto.StoryDTO
+import com.wattpad.ca.ui.MainActivity
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotSame
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +23,38 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    val rule : ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    val appContext = InstrumentationRegistry.getTargetContext()
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.wattpad.ca", appContext.packageName)
     }
+
+
+    @Test
+    fun verifyIfListStoriesLoads(){
+        var listStories: List<StoryDTO>? = StoryController.listStories(appContext)
+        //assertEquals(listStories!!.size, 10)
+        assertNotSame(listStories!!.size, 0)
+    }
+
+    @Test
+    fun verifyIfListStoriesLoadsFromCache(){
+        var listStories: List<StoryDTO>? = StoryController.listStories(appContext)
+        //assertEquals(listStories!!.size, 10)
+        assertNotSame(listStories!!.size, 0)
+    }
+
+    @Test
+    fun simulateClickOnItemList(){
+        Espresso.onView(ViewMatchers.withId(R.id.layBase)).perform(ViewActions.click())
+    }
+
+
 }
